44ba57b1cbfab3ad0dbe32e8a047ed2d12618990
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2022-02-07 15:21:22 -0500

    Make it compatible with node-red-dev

--- settings.js
+++ settings.js
@@ -10,18 +10,7 @@ module.exports = {
         }
       ],
     editorTheme: {
-        /** The following property can be used to set a custom theme for the editor.
-         * See https://github.com/node-red-contrib-themes/theme-collection for
-         * a collection of themes to chose from.
-         */
-        //theme: "",
         palette: {
-            /** The following property can be used to order the categories in the editor
-             * palette. If a node's category is not in the list, the category will get
-             * added to the end of the palette.
-             * If not set, the following default order is used:
-             */
-            //categories: ['subflows', 'common', 'function', 'network', 'sequence', 'parser', 'storage'],
             catalogues: ['https://catalogue.nodered.org/catalogue.json', './catalogue']
         }
     }
